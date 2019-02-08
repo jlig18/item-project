213c155e979c99607d58c06ffe8abe4eb85a3a34
Joseph Liguid <joseph.liguid@outlook.com>
2019-02-08 13:17:07 -0800

    Setup database model

--- models.py
+++ models.py
@@ -1,22 +1,48 @@
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class Item(Base): 
+    __tablename__ = 'item'
 
+    id = Column(Integer, primary_key=True) 
+    name = Column(String(255), nullable=False)  
+    description = Column(String(1024)) 
+    category_id = Column(Integer, ForeignKey('category.id')) 
+    user_id = Column(Integer, ForeignKey('user.id')) 
 
+    @property
+    def serialize(self): 
+        return { 
+            'id' : self.id, 
+            'name': self.name, 
+            'description' : self.description, 
 
+        }
 
+class Category(Base): 
+    __tablename__ = 'category' 
 
+    id = Column(Integer, primary_key=True) 
+    name = Column(String(255), nullable=False) 
 
+    @property 
+    def serialize(self): 
+        return {
+            'id': self.id, 
+            'name': self.name 
+        }
 
+class User(Base): 
+    __tablename__ = 'user' 
 
-
-
-
-
+    id = Column(Integer, primary_key=True) 
+    name = Column(String(255), nullable=False) 
+    email = Column(String(255), nullable=False) 
 
 engine = create_engine('sqlite:///itemcatelog.db') 
+Base.metadata.create_all(engine)
