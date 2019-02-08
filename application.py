333010097c7171fe8bf8027a6958cc71bf81cdfb
Joseph Liguid <joseph.liguid@outlook.com>
2019-02-08 15:50:12 -0800

    Add html templates for each categories and main lists

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 import json 
-from Flask import Flask, jsonify, request, url_for, abort, g, render_template
+from flask import Flask, jsonify, request, url_for, abort, g, render_template, redirect
 from models import Base, Category, Item, User 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
 
-engine = create_engine('sqlite:///itemcatalog.db')
+engine = create_engine('sqlite:///itemcatalog.db', connect_args={'check_same_thread': False}) 
 
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
@@ -16,14 +16,17 @@ app = Flask(__name__)
 @app.route('/')
 def index(): 
     categories = session.query(Category).all() 
-    items = session.query(Item).order_by('Item.id desc').limit(10).all() 
-    render_template('index.html', categories=categories, items=items)  
+    last_items = session.query(Item).order_by('Item.id desc').limit(10).all() 
+    return render_template('index.html', categories=categories, items=last_items)  
 
-@app.route('/<category')
+@app.route('/<category>')
 def showcategory(category): 
     db_category = session.query(Category).filter_by(name=category).first()
-    items = session.query(Item).filter_by(category_id=db_category.id).all() 
-    render_template('showcategory.html', category_name=category, items=items) 
+    if db_category is not None:
+        items = session.query(Item).filter_by(category_id=db_category.id).all() 
+        return render_template('showcategory.html', category_name=db_category.name, items=items) 
+    else:
+        return redirect('/') # TODO: Change to better reponse when accessing non-existent category
 
 
 if __name__ == '__main__':
